527e88283f9248503ae2daf1a48c442db706e65d
Ruh0n <wns8256@naver.com>
2024-01-11 21:55:37 +0900

    :bug: Fix: 과목이 추가되지 않는 문제 해결

--- src/SubjectData.java
+++ src/SubjectData.java
@@ -1,68 +1,30 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class SubjectData {
-    private static List<Subject> subjectStore = new ArrayList<>();
 
-    private static final String SUBJECT_TYPE_MANDATORY = "MANDATORY";
-    private static final String SUBJECT_TYPE_CHOICE = "CHOICE";
+  private static List<Subject> subjectStore;
 
-    private static int subjectIndex;
-    private static final String INDEX_TYPE_SUBJECT = "SU";
+  private static final String SUBJECT_TYPE_MANDATORY = "MANDATORY";
+  private static final String SUBJECT_TYPE_CHOICE = "CHOICE";
 
-    public static void Init() {
-        subjectStore = List.of(
-                new Subject(
-                        INDEX_TYPE_SUBJECT + getSubjectIndex(),
-                        "Java",
-                        SUBJECT_TYPE_MANDATORY
-                ),
-                new Subject(
-                        INDEX_TYPE_SUBJECT + getSubjectIndex(),
-                        "객체지향",
-                        SUBJECT_TYPE_MANDATORY
-                ),
-                new Subject(
-                        INDEX_TYPE_SUBJECT + getSubjectIndex(),
-                        "Spring",
-                        SUBJECT_TYPE_MANDATORY
-                ),
-                new Subject(
-                        INDEX_TYPE_SUBJECT + getSubjectIndex(),
-                        "JPA",
-                        SUBJECT_TYPE_MANDATORY
-                ),
-                new Subject(
-                        INDEX_TYPE_SUBJECT + getSubjectIndex(),
-                        "MySQL",
-                        SUBJECT_TYPE_MANDATORY
-                ),
-                new Subject(
-                        INDEX_TYPE_SUBJECT + getSubjectIndex(),
-                        "디자인 패턴",
-                        SUBJECT_TYPE_CHOICE
-                ),
-                new Subject(
-                        INDEX_TYPE_SUBJECT + getSubjectIndex(),
-                        "Spring Security",
-                        SUBJECT_TYPE_CHOICE
-                ),
-                new Subject(
-                        INDEX_TYPE_SUBJECT + getSubjectIndex(),
-                        "Redis",
-                        SUBJECT_TYPE_CHOICE
-                ),
-                new Subject(
-                        INDEX_TYPE_SUBJECT + getSubjectIndex(),
-                        "MongoDB",
-                        SUBJECT_TYPE_CHOICE
-                )
-        );
-    }
+  private static int subjectIndex;
+  private static final String INDEX_TYPE_SUBJECT = "SU";
 
-    public static String getUID() {
-        return INDEX_TYPE_SUBJECT + getSubjectIndex();
-    }
+  public static void Init() {
+    subjectStore = new ArrayList<>(
+        List.of(new Subject(getNewId(), "Java", SUBJECT_TYPE_MANDATORY),
+                new Subject(getNewId(), "객체지향", SUBJECT_TYPE_MANDATORY),
+                new Subject(getNewId(), "Spring", SUBJECT_TYPE_MANDATORY),
+                new Subject(getNewId(), "JPA", SUBJECT_TYPE_MANDATORY),
+                new Subject(getNewId(), "MySQL", SUBJECT_TYPE_MANDATORY),
+                new Subject(getNewId(), "디자인 패턴", SUBJECT_TYPE_CHOICE),
+                new Subject(getNewId(), "Spring Security", SUBJECT_TYPE_CHOICE),
+                new Subject(getNewId(), "Redis", SUBJECT_TYPE_CHOICE),
+                new Subject(getNewId(), "MongoDB", SUBJECT_TYPE_CHOICE)));
+  }
+
   public static boolean addSubject(Subject subject) {
     // 이미 있다
     if (subjectStore.contains(subject)) {
